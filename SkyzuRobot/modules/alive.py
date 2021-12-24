a0b1d288d677ad720a37bb5c0137d66a2c6eff69
Wahyusaputra <91831925+Wahyu213@users.noreply.github.com>
2021-12-24 09:34:57 +0700

    Create alive.py

---
+++ SkyzuRobot/modules/alive.py
@@ -0,0 +1,30 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from SkyzuRobot.events import register
+from SkyzuRobot import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/91eef6f6dc68d2d5440bd.jpg"
+
+
+@register(pattern=("/alive"))
+async def awake(event):
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Skyzu Robot.** \n\n"
+    TEXT += "⚪ **I'm Working Properly** \n\n"
+    TEXT += f"⚪ **My Master : [Zen](https://t.me/zenfrans)** \n\n"
+    TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
+    TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
+    TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+    TEXT += "**Thanks For Adding Me Here ❤️**"
+    BUTTON = [
+        [
+            Button.url("ʜᴇʟᴘ​", "https://t.me/OkaeriRobot?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/OkaeriUserbot"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
