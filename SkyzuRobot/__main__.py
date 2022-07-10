d7718ac7c95fa006ffb8cd637f31bd70519d8e2c
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2022-07-10 16:13:23 +0700

    Update __main__.py

--- SkyzuRobot/__main__.py
+++ SkyzuRobot/__main__.py
@@ -240,7 +240,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            f"<b>Hi I'm Okaeri robot!</b>\n<b>Started working since:</b> <code>{uptime}</code>",
+            f"<b>Hi I'm Akeno!</b>\n<b>Started working since:</b> <code>{uptime}</code>",
             parse_mode=ParseMode.HTML,
         )
 
